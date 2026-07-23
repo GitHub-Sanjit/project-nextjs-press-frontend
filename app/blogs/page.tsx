b792cd038d55f772fb2795081d0f20ba5554baa6
sanjit <sanjit.est14@gmail.com>
2026-07-24 00:42:59 +0600

    App Router vs Pages Router

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,4 +1,7 @@
-const BlogsPage = () => {
+const BlogsPage = async () => {
+  const post = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const postData = await post.json();
+  console.log(postData);
   return <div>Blogs Page</div>;
 };
 
