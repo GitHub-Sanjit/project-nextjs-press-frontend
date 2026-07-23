7c9e7317817b159a38303782740cf4047bb68d13
sanjit <sanjit.est14@gmail.com>
2026-07-23 23:47:44 +0600

    Linking and Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello World!!</div>;
+  return (
+    <div>
+      Hello World!! Blog Page <Link href={"/blogs"}>Blogs</Link>
+    </div>
+  );
 }
