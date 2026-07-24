34bea65c870ebc6707ce7a95649f66dc73a4e24c
sanjit <sanjit.est14@gmail.com>
2026-07-24 20:31:31 +0600

    Module Starter Pack

--- app/blogs/page.tsx
+++
@@ -1,8 +0,0 @@
-const BlogsPage = async () => {
-  const post = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const postData = await post.json();
-  console.log(postData);
-  return <div>Blogs Page</div>;
-};
-
-export default BlogsPage;
